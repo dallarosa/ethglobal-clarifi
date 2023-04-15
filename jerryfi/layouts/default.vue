01cbfab2b51938258909c37882b0479d6b61cdca
Francisco Dalla Rosa Soares <dallarosa@gmail.com>
2023-04-16 07:57:01 +0900

    Added files for the screens

---
+++ jerryfi/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-app>
+    <v-main>
+        <v-container fluid class="fill-height">
+          <NuxtPage />
+        </v-container>
+      </v-main>
+    </v-app>
+</template>
---
+++ jerryfi/layouts/user.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-navigation-drawer
+        image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        permanent
+        theme="dark"
+      >
+        <v-list nav>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Inbox" value="inbox"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
+          <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 250px"></v-main>
+</template>
