c67f5e2c4f69c7dc01e364c4fab7ca5a440b3d58
Francisco Dalla Rosa Soares <dallarosa@gmail.com>
2023-04-15 20:52:59 +0900

    Added Vuetify

--- jerryfi/nuxt.config.ts
+++ jerryfi/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
+    build: {
+      transpile: ["vuetify"],
+    },
+    vite: {
+      define: {
+        "process.env.DEBUG": false,
+      },
+    },
+  });
